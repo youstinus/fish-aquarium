072e4178fedb0e2da58943b70e062dc95143f8fe
sega <indreee.1997@gmail.com>
2019-11-24 08:15:25 +0200

    add smtp email services, random password generating

--- src/server-core/FishAquariumWebApp/Models/AquariumUser.cs
+++ src/server-core/FishAquariumWebApp/Models/AquariumUser.cs
@@ -1,17 +1,31 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace FishAquariumWebApp.Models
 {
     public class AquariumUser
     {
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime? BirthDate { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        [Required]
         public DateTime? RegistrationDate { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [DataType("Password")]
         public string Password { get; set; }
+        [Required]
         public string Code { get; set; }
-        public int? Type { get; set; }
+        [Required]
+        public int Type { get; set; }
+        [Required]
         public int Id { get; set; }
     }
 }
