6bcc05a8683d5db206d3c2bda2da0006749978f7
sega <indreee.1997@gmail.com>
2019-11-27 22:45:39 +0200

    Login

---
+++ src/server-core/FishAquariumWebApp/Pages/Login.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FishAquariumWebApp.Models;
+
+namespace FishAquariumWebApp.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public string Msg;
+
+        private readonly FishAquariumWebApp.Configurations.FishAquariumContext _context;
+
+        [BindProperty]
+
+        public AquariumUser AquariumUser { get; set; }
+
+        public LoginModel(FishAquariumWebApp.Configurations.FishAquariumContext context)
+        {
+            _context = context;
+        }
+        public void OnGet()
+        {
+        }
+
+        public async System.Threading.Tasks.Task<IActionResult> OnPostAsync(string mail, string password)
+        {
+            AquariumUser = await _context.AquariumUser.FirstOrDefaultAsync(m => m.Email == mail && m.Password == CipherService.Encrypt(password));
+            string a = CipherService.Encrypt(password);
+            if (AquariumUser != null)
+            {
+                HttpContext.Session.SetString("username", AquariumUser.FirstName + " " + AquariumUser.LastName);
+                HttpContext.Session.SetString("role", AquariumUser.Type == 1 ? UserTypes.Admin.ToString() :
+                                                       AquariumUser.Type == 2 ? UserTypes.User.ToString() : UserTypes.Guest.ToString());
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                Msg = "Invalid";
+                return Page();
+            }
+        }
+    }
+}
---
+++ src/server-core/FishAquariumWebApp/Pages/PermissionError.cshtml.cs
@@ -0,0 +1,19 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FishAquariumWebApp.Pages
+{
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public class PermissionErrorModel : PageModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public void OnGet()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        }
+    }
+}
