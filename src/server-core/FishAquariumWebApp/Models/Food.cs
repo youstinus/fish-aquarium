341d13056c74154f4244838ea51924888bdfc714
Edgaras <edgaras.zaliabarstis@gmail.com>
2019-12-11 21:07:53 +0200

    Editet pages and models (Food, Portion, AquariumTask, Food, Parameters)

--- src/server-core/FishAquariumWebApp/Models/Food.cs
+++ src/server-core/FishAquariumWebApp/Models/Food.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace FishAquariumWebApp.Models
 {
+    
+
     public class Food
     {
+        [Required]
         public string Name { get; set; }
         public double? Mass { get; set; }
         public double? Calories { get; set; }
@@ -11,6 +15,7 @@ namespace FishAquariumWebApp.Models
         public double? Proten { get; set; }
         public double? Fat { get; set; }
         public string PrepManual { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime? ExpirationDate { get; set; }
         public int? Allergen { get; set; }
         public int Id { get; set; }
