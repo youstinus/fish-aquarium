dc9b37087f98fbe777c47f7373a2e78fda7d4397
Edgaras <edgaras.zaliabarstis@gmail.com>
2019-12-11 21:07:53 +0200

    Editet pages and models (Food, Portion, AquariumTask, Food, Parameters)

--- src/server-core/FishAquariumWebApp/Models/Portion.cs
+++ src/server-core/FishAquariumWebApp/Models/Portion.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace FishAquariumWebApp.Models
 {
     public class Portion
     {
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime? PreparationDate { get; set; }
         public int Id { get; set; }
         public int? FkFood { get; set; }
