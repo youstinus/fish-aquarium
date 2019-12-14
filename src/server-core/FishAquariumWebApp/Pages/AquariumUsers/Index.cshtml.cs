efda24a32eb4794af0ac469966ac1b800f193c0c
sega <indreee.1997@gmail.com>
2019-12-14 14:54:01 +0200

    users' filter

--- src/server-core/FishAquariumWebApp/Pages/AquariumUsers/Index.cshtml.cs
+++ src/server-core/FishAquariumWebApp/Pages/AquariumUsers/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using FishAquariumWebApp.Configurations;
 using FishAquariumWebApp.Models;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace FishAquariumWebApp.Pages.AquariumUsers
 {
@@ -14,16 +15,71 @@ namespace FishAquariumWebApp.Pages.AquariumUsers
     {
         private readonly FishAquariumWebApp.Configurations.FishAquariumContext _context;
 
+
         public IndexModel(FishAquariumWebApp.Configurations.FishAquariumContext context)
         {
             _context = context;
         }
 
-        public IList<AquariumUser> AquariumUsers { get;set; }
+        //public IList<AquariumUser> AquariumUsers { get; set; }
+        public IList<AquariumUserTasks> AquariumUsers { get; set; }
 
-        public async Task OnGetAsync()
+        public int filter { get; set; }
+        private string level { get; set; }
+
+        public async Task OnGetAsync(string filter)
         {
-            AquariumUsers = await _context.AquariumUser.ToListAsync();
+            int tasksCount = await _context.AquariumTask.CountAsync();
+            int userCount = await _context.AquariumUser.CountAsync();
+            int filt = tasksCount / userCount;
+
+
+            AquariumUsers = (from users in _context.AquariumUser
+                     join tasks in _context.AquariumTask on users.Id equals tasks.FkAquariumUser into usersTasks
+
+                     select new AquariumUserTasks
+                     {
+                        FirstName = users.FirstName,
+                        LastName = users.LastName,
+                        BirthDate = users.BirthDate,
+                        RegistrationDate = users.RegistrationDate,
+                        Email = users.Email,
+                        Code = users.Code,
+                        Type = users.Type,
+                        TaskCount = usersTasks.Count()
+
+           }).ToList();
+     
+            switch (filter)
+            {
+                case "1":
+                    AquariumUsers = AquariumUsers.Where(x => x.TaskCount > ( filt * 2 / 3)).ToList();
+                    break;
+                case "2":
+                    AquariumUsers = AquariumUsers.Where(x => x.TaskCount > (filt /  3) && x.TaskCount <= (filt * 2 / 3)).ToList();
+
+                    break;
+                case "3":
+                    AquariumUsers = AquariumUsers.Where(x => x.TaskCount <= (filt / 3)).ToList();
+
+                    break;
+                default:  
+                    break;
+            }
         }
+
+    }
+
+    public class AquariumUserTasks
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public DateTime? RegistrationDate { get; set; }
+        public string Email { get; set; }
+        public string Code { get; set; }
+        public int Type { get; set; }
+        public int TaskCount { get; set; }
+        public int Id { get; set; }
     }
 }
